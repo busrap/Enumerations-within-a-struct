44acf3aa59f52d35a0f93492985dc410f347048e
busra06 <busra.polat.tr@gmail.com>
2021-01-05 14:42:15 +0300

    struch_enum

---
+++ struch_enum/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace struch_enum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.Adi = "selma";
+            ogr.Soyadi = "kutlu";
+            Ders ders = new Ders { DersAdi = "matematik", Final = 50, Vize = 60 };
+            Ders ders2 = new Ders { DersAdi = "Fizik", Final = 80, Vize = 90 };
+            Ders ders3 = new Ders { DersAdi = "kimya", Final = 60, Vize = 80 };
+            ogr.alani = Alan.sayisal;
+
+            switch (ogr.rk)
+            {
+                case renk.kırmızı:
+                    
+
+                    break;
+                case renk.sarı:
+                    break;
+                case renk.mavi:
+                    break;
+                case renk.lacivert:
+                    break;
+                case renk.mor:
+                    break;
+                case renk.pembe:
+                    break;
+                default:
+                    break;
+            }
+
+            ogr.Dersler.Add(ders);
+            ogr.Dersler.Add(ders2);
+            ogr.Dersler.Add(ders3);
+
+
+        }
+        // struct class a göre daha kolay çalışa bilen bir nesnedir
+        //struct lar ayrıca tanımlanan yapılar değildir oluşturulan calass'ın altında üstünde bir yerinde tanımlanan yapılardır.
+    }
+}
---
+++ struch_enum/Ogrenci.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struch_enum
+{
+    class Ogrenci    /// class ve struct arasında ki fark "ogrenci class'ı miras olarak alına bilir fakat Ders(struct) miras olarak alınamaz
+    {
+        public Ogrenci()
+        {
+            Dersler = new List<Ders>();
+        }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public List<Ders> Dersler { get; set; }
+        public  Alan alani{ get; set; }
+        public Renk rk { get; set; }
+
+
+
+
+
+        public decimal Ortalama
+           
+        {
+            get
+            {
+                decimal toplam = 0;
+                foreach (Ders not in Dersler)
+                {
+                    toplam += not.Ortalama;
+                }
+                return toplam / Dersler.Count();
+
+            }
+
+        }
+       
+
+    }
+    enum Renk
+    {
+        kırmızı, sarı, mavi, lacivert, mor, pembe
+
+    }
+
+    enum Alan
+    {
+        EsitAgirlik,
+        sayisal,
+        sozel,
+        dil
+    }
+
+    struct Ders
+    {
+        public string DersAdi { get; set; }
+        public decimal Vize { get; set; }
+        public decimal Final { get; set; }
+        public decimal Ortalama
+        {
+            get
+            {
+                return Vize * 0.4m + Final * 0.6m;
+            }
+        }
+
+    }
+}
